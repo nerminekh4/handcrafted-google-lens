e9de29a39da50ca5b0c56cd578e11d08cecbee25
Nermine khadhraoui <nerminekhadhraoui4@gmail.com>
2026-06-08 11:27:14 +0200

    refactor: the implementation of the backend is following the oop pattern

--- backend.py
+++ backend.py
@@ -2,43 +2,49 @@ from groq import Groq
 from dotenv import load_dotenv
 import os 
 
-load_dotenv()
 
-client = Groq(api_key=os.environ["GROQ_API_KEY"])
-
-
-
-def read_file(file_path):
-	with open(file_path, "r") as file:
-		return file.read()
+class LlmAgent:
+    def __init__(self, ):
+        load_dotenv()
+        self.client = Groq(api_key=os.environ["GROQ_API_KEY"])
 
 
+    @staticmethod
+    def read_file(file_path):
+        with open(file_path, "r") as file:
+            return file.read()
 
 
-def ask_llm(user_interaction):
+    def ask_llm(self, user_interaction):
 
-	chat_completion = client.chat.completions.create(
-		messages=[
-			{
-				"role": "system",
-				"content": read_file(file_path="./context.txt")
-			},
-			{
-				"role": "user",
-				"content": user_interaction
-			}
-		],
+        chat_completion = self.client.chat.completions.create(
+            messages=[
+                {
+                    "role": "system",
+                    "content": LlmAgent.read_file(file_path="./context.txt")
+                },
+                {
+                    "role": "user",
+                    "content": user_interaction
+                }
+            ],
 
-		model="llama-3.3-70b-versatile"
-	)
+            model="llama-3.3-70b-versatile"
+        )
 
 
-	return chat_completion.choices[0].message.content
+        return chat_completion.choices[0].message.content
 
 
 
 if __name__ == "__main__":
+	llm_agent_object = LlmAgent()
+
+
 	user_interaction = input("Posez votre question :\n")
-	llm_response = ask_llm(user_interaction)
+	
+	llm_response = llm_agent_object.ask_llm(user_interaction)
+	
+
 	print("-"*20)
 	print(llm_response)
