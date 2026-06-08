8ce38ccf7d2b50b1aba9276c10dbe2af02b02319
Nermine khadhraoui <nerminekhadhraoui4@gmail.com>
2026-06-08 11:04:00 +0200

    now we can ask a question to llm

--- backend.py
+++ backend.py
@@ -1,2 +1,44 @@
+from groq import Groq
 from dotenv import load_dotenv
-import os
+import os 
+
+load_dotenv()
+
+client = Groq(api_key=os.environ["GROQ_API_KEY"])
+
+
+
+def read_file(file_path):
+	with open(file_path, "r") as file:
+		return file.read()
+
+
+
+
+def ask_llm(user_interaction):
+
+	chat_completion = client.chat.completions.create(
+		messages=[
+			{
+				"role": "system",
+				"content": read_file(file_path="./context.txt")
+			},
+			{
+				"role": "user",
+				"content": user_interaction
+			}
+		],
+
+		model="llama-3.3-70b-versatile"
+	)
+
+
+	return chat_completion.choices[0].message.content
+
+
+
+if __name__ == "__main__":
+	user_interaction = input("Posez votre question :\n")
+	llm_response = ask_llm(user_interaction)
+	print("-"*20)
+	print(llm_response)
